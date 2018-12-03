7b41b55ae731cafd99adbb9ea87bcf9f639bd6b5
Anthony Todek <tonytodek@yahoo.com>
2018-12-03 18:55:20 -0500

    Updated from DB

--- Tra-Verse/Models/Tra-Verse.Designer.cs
+++ Tra-Verse/Models/Tra-Verse.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\johna\Desktop\Tra-Verse\Tra-Verse\Tra-Verse\Models\Tra-Verse.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\tonyt\source\repos\Tra-Verse\Tra-Verse\Models\Tra-Verse.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
