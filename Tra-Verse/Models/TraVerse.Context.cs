7b57a9c2abb7dba69cbcdb91101fdb84db82b778
AndreaDabrowski <AnnMDabrowski@gmail.com>
2018-12-04 19:30:45 -0500

    added new DB

--- Tra-Verse/Models/TraVerse.Context.cs
+++ Tra-Verse/Models/TraVerse.Context.cs
@@ -9,23 +9,23 @@
 
 namespace Tra_Verse.Models
 {
+    using System;
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
-
+    
     public partial class TraVerseEntities : DbContext
     {
         public TraVerseEntities()
             : base("name=TraVerseEntities")
         {
         }
-
+    
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             throw new UnintentionalCodeFirstException();
         }
-
+    
         public virtual DbSet<User> Users { get; set; }
         public virtual DbSet<VacationLog> VacationLogs { get; set; }
-        
     }
 }
