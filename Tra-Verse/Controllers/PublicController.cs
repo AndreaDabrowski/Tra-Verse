5d8f07a0b125271609542f201d3ce902a77f037b
AndreaDabrowski <AnnMDabrowski@gmail.com>
2018-12-07 15:54:54 -0500

    starting public functionalities

---
+++ Tra-Verse/Controllers/PublicController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tra_Verse.Controllers
+{
+    public class PublicController : Controller
+    {
+        HomeController HC = new HomeController();
+        // GET: Public
+        public ActionResult PublicAccomodations()
+        {
+            HC.Travel();
+            HC.NASA();
+            return View();
+        }
+        public ActionResult PublicTripList()
+        {
+            ViewBag.Travel = HC.Travel();
+            ViewBag.NASA = HC.NASA();
+            return View();
+        }
+    }
+}
