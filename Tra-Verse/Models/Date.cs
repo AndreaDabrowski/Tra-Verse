4cd74b9faa4ac6dc1067dd821461e09da6e1e1cf
Anthony Todek <tonytodek@yahoo.com>
2018-12-12 09:11:28 -0500

    made changes to the form buttons

--- Tra-Verse/Models/Date.cs
+++ Tra-Verse/Models/Date.cs
@@ -7,16 +7,16 @@ namespace Tra_Verse.Models
 {
     public class Date
     {
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
+        public DateTime DateStart { get; set; }
+        public DateTime DateEnd { get; set; }
         //public static  { get; set; }
 
         public Date()
         {
-            StartDate = new DateTime(8018, 12, 14);
+            DateStart = new DateTime(8018, 12, 14);
             Random random = new Random();
             int randNum = random.Next(30, 280);
-            EndDate = StartDate.AddDays(randNum);
+            DateEnd = DateStart.AddDays(randNum);
             //int range = (start).Days;            
         }
     }
