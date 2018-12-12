3d7d4e10f01634c69749ddd30b17850369ddba8e
AndreaDabrowski <AnnMDabrowski@gmail.com>
2018-12-12 02:36:42 -0500

    FINALLY FIXED CHECKOUT ERROR

--- Tra-Verse/Controllers/UserController.cs
+++ Tra-Verse/Controllers/UserController.cs
@@ -98,59 +98,59 @@ namespace Tra_Verse.Controllers
         //    return View();
         //}
 
-        
-        public ActionResult TotalPrice(FormCollection form)
-        {
-            string shipType = form["ShipType"];               ViewBag.ShipType = shipType;
-            string exoSuit = form["ExoSuit"];                 ViewBag.ExoSuit = exoSuit;
-            string dateEnd = form["DateEnd"];                 ViewBag.DateEnd = dateEnd;
-            string dateStart = form["DateStart"];             ViewBag.DateStart = dateStart;
-            string rating = form["Rating"];                   ViewBag.Rating = rating;
-            string planetName = form["PlanetName"];           ViewBag.PlName = planetName;
-            string refundable = form["Refundable"];           ViewBag.Refundable = refundable;
-            string companyName = form["CompanyName"];         ViewBag.CompanyName = companyName;
-            string planetIndex = form["PlanetIndex"];         ViewBag.PlanetIndex = planetIndex;
-            string companyIndex = form["CompanyIndex"];       ViewBag.CompanyIndex = companyIndex;
-            string travelIndex = form["TravelIndex"];         ViewBag.TravelIndex = travelIndex;
-            int basePrice = int.Parse(form["BasePrice"]);
 
-            int newGrandTotal = Calculation.TotalPrice(shipType, exoSuit, basePrice, rating);
-            VacationLog price = database.VacationLogs.Find(currentUser.OrderID);
-            price.Price = newGrandTotal;
-            ViewBag.TotalPrice = price.Price;
-
-
-            return View();
-        }
-
-
-        //public ActionResult TotalPrice(FormCollection variables)
+        //public ActionResult TotalPrice(FormCollection form)
         //{
-        //    ViewBag.ShipType = variables["ShipType"];
-        //    ViewBag.Exosuit = variables["ExoSuit"];
-        //    ViewBag.DateEnd = variables["DateEnd"];
-        //    ViewBag.DateStart = variables["DateStart"];
-        //    ViewBag.Rating = variables["Rating"];//need
-        //    ViewBag.PlanetName = variables["PlanetName"];
-        //    ViewBag.Refundable = variables["Refundable"];
-        //    ViewBag.CompanyName = variables["CompanyName"];
+        //    string shipType = form["ShipType"];               ViewBag.ShipType = shipType;
+        //    string exoSuit = form["ExoSuit"];                 ViewBag.ExoSuit = exoSuit;
+        //    string dateEnd = form["DateEnd"];                 ViewBag.DateEnd = dateEnd;
+        //    string dateStart = form["DateStart"];             ViewBag.DateStart = dateStart;
+        //    string rating = form["Rating"];                   ViewBag.Rating = rating;
+        //    string planetName = form["PlanetName"];           ViewBag.PlName = planetName;
+        //    string refundable = form["Refundable"];           ViewBag.Refundable = refundable;
+        //    string companyName = form["CompanyName"];         ViewBag.CompanyName = companyName;
+        //    string planetIndex = form["PlanetIndex"];         ViewBag.PlanetIndex = planetIndex;
+        //    string companyIndex = form["CompanyIndex"];       ViewBag.CompanyIndex = companyIndex;
+        //    string travelIndex = form["TravelIndex"];         ViewBag.TravelIndex = travelIndex;
+        //    int basePrice = int.Parse(form["BasePrice"]);
 
-        //    ViewBag.PlanetIndex = variables["PlanetIndex"];
-        //    ViewBag.CompanyIndex = variables["CompanyIndex"];
-        //    ViewBag.TravelIndex = variables["TravelIndex"];
+        //    int newGrandTotal = Calculation.TotalPrice(shipType, exoSuit, basePrice, rating);
+        //    VacationLog price = database.VacationLogs.Find(currentUser.OrderID);
+        //    price.Price = newGrandTotal;
+        //    ViewBag.TotalPrice = price.Price;
 
-        //    string shipType = ViewBag.ShipType;
-        //    string exoSuit = ViewBag.Exosuit;
-        //    string rating = ViewBag.Rating;
 
-        //    string test = variables["BasePrice"].ToString();
-
-        //    //int pr = int.Parse(test);
-        //    //ViewBag.RefreshedTotal = Calculation.TotalPrice(variables["ShipType"].ToString(), variables["ExoSuit"].ToString(), pr, variables["Rating"].ToString());
-        //    ViewBag.RefreshedTotal = Calculation.TotalPrice(shipType, exoSuit, 5, rating);
-        //    ViewBag.PlanetPic = TripListObject.Planets();
-        //    return View(variables);//how to send trip indices
+        //    return View();
         //}
+
+
+        public ActionResult TotalPrice(FormCollection variables)
+        {
+            ViewBag.ShipType = variables["ShipType"];
+            ViewBag.Exosuit = variables["ExoSuit"];
+            ViewBag.DateEnd = variables["DateEnd"];
+            ViewBag.DateStart = variables["DateStart"];
+            ViewBag.Rating = variables["Rating"];//need
+            ViewBag.PlanetName = variables["PlanetName"];
+            ViewBag.Refundable = variables["Refundable"];
+            ViewBag.CompanyName = variables["CompanyName"];
+
+            ViewBag.PlanetIndex = variables["PlanetIndex"];
+            ViewBag.CompanyIndex = variables["CompanyIndex"];
+            ViewBag.TravelIndex = variables["TravelIndex"];
+
+            string shipType = ViewBag.ShipType;
+            string exoSuit = ViewBag.Exosuit;
+            string rating = ViewBag.Rating;
+
+            string test = variables["BasePrice"].ToString();
+
+            int pr = int.Parse(test);
+            ViewBag.RefreshedTotal = Calculation.TotalPrice(variables["ShipType"].ToString(), variables["ExoSuit"].ToString(), pr, variables["Rating"].ToString());
+            //ViewBag.RefreshedTotal = Calculation.TotalPrice(shipType, exoSuit, 5, rating);
+            ViewBag.PlanetPic = TripListObject.Planets();
+            return View();//how to send trip indices
+        }
 
         public ActionResult Login()
         {
